df6fcc00562195a815e1e4d633391cfde6bda698
Анна Сиряк <grafova.a.i@gmail.com>
2022-06-10 00:58:08 +0300

    Добавила вторую функцию

--- js/main.js
+++ js/main.js
@@ -12,11 +12,36 @@ function getRandomInteger (min, max) {
   if(max >= min) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   } else {
-      console.log('Ooooops!');
+      return 'Ooooops!';
   } //Вообще не знаю, что здесь придумать!
 }
 
-getRandomInteger(1, 0);
+getRandomInteger();
 
 /* Функцию подсмотрела здесь:
 https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random */
+
+/* Функция для проверки максимальной длины строки
+
+- будет использоваться для проверки длины введённого комментария
+- должна быть универсальна
+- результат: true, если строка проходит по длине
+- false — если не проходит
+
+имя_функции(проверяемая_строка, максимальная_длина) */
+
+function getStringLength (comment, maxL) {
+  comment = 'Текст комментария';
+  maxL = 100;
+
+  if(comment.length <= maxL) {
+    return true;
+  } else {
+      return false;
+  }
+}
+
+getStringLength()
+
+/* Про length посмотрела здесь:
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length */
